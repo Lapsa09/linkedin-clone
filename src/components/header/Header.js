7db57cfbf9474a8116ce33a4579a4b2d6729e730
Lapsa <agusdinar@hotmail.com>
2021-07-14 19:10:34 -0300

    responsive pt2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,9 @@ import {
   Notifications,
   Search,
   SupervisorAccount,
+  MoreHoriz,
 } from "@material-ui/icons";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HeaderOption from "../header-option/HeaderOption";
 import "./header.css";
 import Menu from "../menu/Menu";
@@ -20,8 +21,46 @@ function Header() {
 
   const { width } = useSelector(getWidth);
 
+  const hiddenButtonsRef = useRef(null);
+  const hiddenSearchRef = useRef(null);
+
+  const [showIcons, setShowIcons] = useState(false);
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
   const goHome = () => {
     history.push("/");
+  };
+
+  useEffect(() => {
+    const pageClickEventA = (e) => {
+      if (hiddenButtonsRef.current !== null) toggleIcons();
+    };
+
+    if (showIcons) {
+      window.addEventListener("click", pageClickEventA);
+    }
+
+    return () => window.removeEventListener("click", pageClickEventA);
+  }, [showIcons]);
+
+  useEffect(() => {
+    const pageClickEventB = (e) => {
+      if (hiddenSearchRef.current !== null) toggleSearchBar();
+    };
+
+    if (showSearchBar) {
+      window.addEventListener("click", pageClickEventB);
+    }
+
+    return () => window.removeEventListener("click", pageClickEventB);
+  }, [showSearchBar]);
+
+  const toggleIcons = () => {
+    setShowIcons(!showIcons);
+  };
+
+  const toggleSearchBar = () => {
+    setShowSearchBar(!showSearchBar);
   };
   return (
     <div className="header">
@@ -34,18 +73,43 @@ function Header() {
         {width > 675 ? (
           <SearchBar />
         ) : (
-          <HeaderOption Icon={Search} title="Search" />
+          <>
+            <HeaderOption
+              onClick={toggleSearchBar}
+              Icon={Search}
+              title="Search"
+            />
+            {showSearchBar && (
+              <div ref={hiddenSearchRef} className="hiddenSearchBar">
+                <SearchBar />
+              </div>
+            )}
+          </>
         )}
       </div>
 
       <div className="header__right">
         <HeaderOption Icon={Home} title="Home" onClick={goHome} />
-
         <HeaderOption Icon={SupervisorAccount} title="My Network" />
         <HeaderOption Icon={BusinessCenter} title="Jobs" />
-        <HeaderOption Icon={Chat} title="Messaging" />
-        <HeaderOption Icon={Notifications} title="Notifications" />
-        <Menu />
+        {width > 500 ? (
+          <>
+            <HeaderOption Icon={Chat} title="Messaging" />
+            <HeaderOption Icon={Notifications} title="Notifications" />
+            <Menu />
+          </>
+        ) : (
+          <>
+            <HeaderOption Icon={MoreHoriz} onClick={toggleIcons} />
+            {showIcons && (
+              <div ref={hiddenButtonsRef} className="header__hiddenButtons">
+                <HeaderOption Icon={Chat} title="Messaging" />
+                <HeaderOption Icon={Notifications} title="Notifications" />
+                <Menu />
+              </div>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
